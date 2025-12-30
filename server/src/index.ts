bdaea8fdc2635d95d300b07cda03678b10326280
sleep-written <sleep.written0000@gmail.com>
2025-12-30 17:17:20 -0300

    Changed how the argv is parsed.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,36 @@
-import { Mocks } from '@mocks/index.js';
+import { parseArgs, styleText } from 'node:util';
 import { Server } from '@pm2-logger/utils-server';
 
 import { endpointsRouting } from './endpoints/routing.js';
+import { Mocks } from './mocks/index.js';
 
-const mocks = new Mocks();
+const { values } = parseArgs({
+    options: {
+        port: {
+            type: 'string',
+            short: 'p',
+            default: '8080',
+            multiple: false
+        },
+        mocks: {
+            type: 'string',
+            short: 'm',
+            default: [],
+            multiple: true
+        }
+    }
+});
+
+const mocks = new Mocks(values.mocks);
 mocks.execute();
 
 const app = new Server();
 app.use(endpointsRouting);
 
-const server = app.listen(8080, () => {
-    console.log('ready!');
+const server = app.listen(values.port, () => {
+    const styledPort = styleText('blueBright', values.port);
+    console.log(`The server is listening on port ${styledPort}.-`);
+
     process.once('SIGINT', () => {
         server.close();
     });
--- server/src/mocks/mocks.ts
+++ server/src/mocks/mocks.ts
@@ -1,30 +1,26 @@
-import { parseArgs, styleText } from 'node:util';
+import { styleText } from 'node:util';
 import { Server } from '@pm2-logger/utils-server';
 
 import { PM2Mock, type PM2Object } from './pm2/index.js';
 import { PM2 } from '@pm2-logger/utils-pm2';
 
 export class Mocks {
-    #mocks: Record<string, () => void> = {
+    #callbacks: Record<string, () => void> = {
         pm2: () => {
             Server.factory<PM2Object>(PM2, () => new PM2Mock());
         }
     };
 
-    execute(): void {
-        const { values: { mocks } } = parseArgs({
-            options: {
-                mocks: {
-                    type: 'string',
-                    short: 'm',
-                    multiple: true
-                }
-            }
-        });
+    #mocks: string[];
 
+    constructor(mocks: string[]) {
+        this.#mocks = mocks;
+    }
+
+    execute(): void {
         Object
-            .entries(this.#mocks)
-            .filter(([ k ]) => mocks?.includes(k))
+            .entries(this.#callbacks)
+            .filter(([ k ]) => this.#mocks?.includes(k))
             .forEach(([ name, callback ]) => {
                 callback();
 
